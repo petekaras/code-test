3642f07ee89e9353501b3f2cabdf93193e77e121
jkosha <nikita.koselev@gmail.com>
2021-01-23 17:09:58 +0000

    80% ready solution, tests fail

--- src/main/java/practice/Snail.java
+++ src/main/java/practice/Snail.java
@@ -1,14 +1,59 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
  * array = [[1,2,3],
- *          [4,5,6],
- *          [7,8,9]]
+ * [4,5,6],
+ * [7,8,9]]
  * snail(array) = [1,2,3,6,9,8,7,4,5]
  */
 public class Snail {
-    public int[] snail(int[][] grid){
-        return new int[]{};
+    public int[] snail(int[][] grid) {
+        int leftLimit = 0;
+        int upperLimit = 0;
+        int rightLimit = grid[0].length;
+        int lowerLimit = grid.length;
+
+        List<Integer> result = new ArrayList<>();
+
+        while (conditionsAreMet(leftLimit, upperLimit, rightLimit, lowerLimit)) {
+
+            for (int i = leftLimit; i < rightLimit; i++) {
+                result.add(grid[upperLimit][i]);
+            }
+            upperLimit++;
+
+            if (conditionsAreMet(leftLimit, upperLimit, rightLimit, lowerLimit)) {
+                for (int i = upperLimit; i < lowerLimit; i++) {
+                    result.add(grid[i][rightLimit]);
+                }
+                rightLimit--;
+            }
+
+
+            if (conditionsAreMet(leftLimit, upperLimit, rightLimit, lowerLimit)) {
+                for (int i = rightLimit; i > leftLimit; i--) {
+                    result.add(grid[lowerLimit][i]);
+                }
+                lowerLimit--;
+            }
+
+            if (conditionsAreMet(leftLimit, upperLimit, rightLimit, lowerLimit)) {
+                for (int i = lowerLimit; i > upperLimit; i--) {
+                    result.add(grid[i][leftLimit]);
+                }
+                leftLimit++;
+            }
+        }
+
+        int[] ints = result.stream().mapToInt(i -> i).toArray();
+        return ints;
+    }
+
+    private boolean conditionsAreMet(int leftLimit, int upperLimit, int rightLimit, int lowerLimit) {
+        return leftLimit < rightLimit && upperLimit < lowerLimit;
     }
 }
--- src/test/java/practice/SnailTest.java
+++ src/test/java/practice/SnailTest.java
@@ -2,9 +2,26 @@ package practice;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SnailTest {
 
+
+    private Snail snail = new Snail();
+
     @Test
-    void snail() {
+    void simple3By3Array() {
+        final int[] expectedResult = new int[]{1, 2, 3, 6, 9, 8, 7, 4, 5};
+        int[] result = snail.snail(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void simple1DArray() {
+        final int[] expectedResult = new int[]{1, 2, 3, 4, 5};
+        int[] result = snail.snail(new int[][]{{1, 2, 3, 4, 5}});
+
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
